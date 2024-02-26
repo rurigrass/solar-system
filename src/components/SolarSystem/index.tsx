8c91002fcbadd8a21e0af9fecf1a3fd2136fd13c
rurigrass <ruripg@gmail.com>
2024-02-26 13:21:22 +0100

    better late than never

--- src/components/SolarSystem/index.tsx
+++ src/components/SolarSystem/index.tsx
@@ -22,42 +22,42 @@ export default function SolarSystem({
     sun: { scale: { actual: 109, fun: 25 } },
     mercury: {
       scale: { actual: 0.2, fun: 1 },
-      radius: { actual: 10, fun: 15 },
+      radius: { actual: 35, fun: 15 },
       speed: { actual: 1.607 },
     },
     venus: {
       scale: { actual: 0.949, fun: 4.9 },
-      radius: { actual: 10, fun: 25 },
+      radius: { actual: 44.62, fun: 25 },
       speed: { actual: 1.176 },
     },
     earth: {
       scale: { actual: 1, fun: 5 },
-      radius: { actual: 10, fun: 35 },
+      radius: { actual: 54.48, fun: 35 },
       speed: { actual: 1 },
     },
     mars: {
       scale: { actual: 0.5, fun: 2.5 },
-      radius: { actual: 10, fun: 45 },
+      radius: { actual: 72.49, fun: 45 },
       speed: { actual: 0.808 },
     },
     jupiter: {
       scale: { actual: 11, fun: 15 },
-      radius: { actual: 10, fun: 60 },
+      radius: { actual: 119.55, fun: 60 },
       speed: { actual: 0.439 },
     },
     saturn: {
       scale: { actual: 10, fun: 13 },
-      radius: { actual: 10, fun: 75 },
+      radius: { actual: 149.68, fun: 75 },
       speed: { actual: 0.325 },
     },
     uranus: {
       scale: { actual: 4.1, fun: 9.5 },
-      radius: { actual: 10, fun: 90 },
+      radius: { actual: 175.29, fun: 90 },
       speed: { actual: 0.228 },
     },
     neptune: {
       scale: { actual: 4, fun: 9 },
-      radius: { actual: 10, fun: 100 },
+      radius: { actual: 204.98, fun: 100 },
       speed: { actual: 0.181 },
     },
   };
